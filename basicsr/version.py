895b0289789ec644c86d847188b94f8125e28f6f
unknown <naveenvarshney2000@gmail.com>
2026-02-24 16:17:47 +0530

    Final

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Tue Feb 17 10:28:05 2026
-__version__ = '1.2.0+unknown'
+# TIME: Tue Feb 24 16:09:57 2026
+__version__ = '1.2.0+06efce8'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
